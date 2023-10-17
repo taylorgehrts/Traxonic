ee4d5e5bb7a9abe0185a213388379435c43929ab
taylorgehrts <taylorgehrts@gmail.com>
2023-10-18 00:29:36 +0200

    files page is done enough, you can upload, download and delete files and play audio files

--- client/src/components/FileUtils.jsx
+++ client/src/components/FileUtils.jsx
@@ -1,6 +1,6 @@
 import { Storage } from 'aws-amplify';
 
-import { useParams } from 'react-router-dom';
+// import { useParams } from 'react-router-dom';
 
 export const handleFileUpload = async (event, realProjectId) => {
   const file = event.target.files[0];
@@ -13,19 +13,18 @@ export const handleFileUpload = async (event, realProjectId) => {
     const result = await Storage.put(s3Key, file);
     
     // Store the file reference in MongoDB
-    const fileData = {
-      title: file.name,
-      url: result.key, // Use the S3 key as the URL
-      
-      size: file.size,
-      uploadedOn: new Date(),
-      projectId: projectId, // Include the projectId in the MongoDB document
-    };
+    // const fileData = {
+    //   title: file.name,
+    //   url: result.key, // Use the S3 key as the URL
+    //   size: file.size,
+    //   uploadedOn: new Date(),
+    //   projectId: projectId, // Include the projectId in the MongoDB document
+    // };
 
-    // Save the file reference to MongoDB
-    const newFile = await File.create(fileData);
+    // // Save the file reference to MongoDB
+    // const newFile = await File.create(fileData);
 
-    console.log('File uploaded successfully:', newFile);
+    console.log('File uploaded successfully:', s3Key);
   } catch (error) {
     console.error('Error uploading file:', error);
   }
--- client/src/components/Files.jsx
+++ client/src/components/Files.jsx
@@ -1,10 +1,9 @@
-import React, { useState } from "react";
-import { handleFileUpload } from "./FileUtils";
+import React, { useState, useEffect, useRef } from "react";
+import { Storage } from "aws-amplify";
 import {
   Button,
   Container,
   Typography,
-  Paper,
   List,
   ListItem,
   ListItemText,
@@ -15,65 +14,213 @@ import {
   useMediaQuery,
   Box,
 } from "@mui/material";
+
 import DeleteIcon from "@mui/icons-material/Delete";
 import DownloadIcon from "@mui/icons-material/Download";
+import PlayArrowIcon from "@mui/icons-material/PlayArrow";
+import PauseIcon from "@mui/icons-material/Pause";
 import theme from "../theme";
+import { handleFileUpload } from "./fileUtils";
 
 function Files({ projectId, realProjectId }) {
   console.log("Project ID in the files.jsx:", projectId);
   console.log("Real Project ID in the Files.jsx:", realProjectId);
 
-  // Sample file data for illustration purposes
-  const files = [
-    { id: 1, name: "File 1", duration: "3:45", size: "5MB" },
-    { id: 2, name: "File 2", duration: "2:30", size: "3MB" },
-    // ... add more file data as needed
-  ];
-
+  const [files, setFiles] = useState([]);
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const audioPlayerRef = useRef(null);
+  const [playingFile, setPlayingFile] = useState(null);
+  const [audio, setAudio] = useState(new Audio());
+  const [audioUrl, setAudioUrl] = useState(null);
 
-  const handleCheckboxChange = (fileId) => {
+  useEffect(() => {
+    // Cleanup the audio player when the component is unmounted
+    return () => {
+      if (audioPlayerRef.current) {
+        audioPlayerRef.current.pause();
+        audioPlayerRef.current.src = "";
+      }
+    };
+  }, []);
+
+  const handleCheckboxChange = (fileKey) => {
     setSelectedFiles((prevSelected) => {
-      if (prevSelected.includes(fileId)) {
+      if (prevSelected.includes(fileKey)) {
         // Deselect file
-        return prevSelected.filter((id) => id !== fileId);
+        return prevSelected.filter((key) => key !== fileKey);
       } else {
         // Select file
-        return [...prevSelected, fileId];
+        return [...prevSelected, fileKey];
       }
     });
   };
 
-  const handleDeleteIconClick = () => {
-    // Handle deletion logic here
-    console.log("Selected files to delete:", selectedFiles);
+  const handlePlayPause = async (fileKey) => {
+    try {
+      const fileName = extractFileName(fileKey);
+      const newAudioUrl = await Storage.get(`projects/${realProjectId}/${fileName}`);
 
-    // Assuming you have a function to remove files
-    // handleRemoveFile function needs to be implemented
-    selectedFiles.forEach((fileId) => {
-      handleRemoveFile(fileId);
-    });
-
-    // Clear selected files after deletion
-    setSelectedFiles([]);
+      if (playingFile === fileKey) {
+        // Pause if the same file is playing
+        audio.pause();
+        setPlayingFile(null);
+      } else {
+        // Play if a different file is selected
+        if (audioUrl !== newAudioUrl) {
+          // Set the new audio source only if it's different
+          setAudioUrl(newAudioUrl);
+          audio.src = newAudioUrl;
+        }
+        audio.play();
+        setPlayingFile(fileKey);
+      }
+    } catch (error) {
+      console.error("Error playing audio:", error);
+    }
   };
 
-  const handleDownloadIconClick = () => {
-    // Handle download logic here
-    console.log("Selected files to download:", selectedFiles);
-    // Implement the logic to download selected files
+  useEffect(() => {
+    // Cleanup the audio player when the component is unmounted
+    return () => {
+      audio.pause();
+      audio.src = "";
+    };
+  }, [audio]); 
+  
+  const isAudioFile = (fileKey) => {
+    // Extract file extension
+    const fileExtension = fileKey.split('.').pop();
+
+    // List of audio file extensions you want to support
+    const audioExtensions = ['mp3', 'wav', 'ogg', 'aac', 'flac'];
+
+    // Check if the file extension is in the list of audio file extensions
+    return audioExtensions.includes(fileExtension.toLowerCase());
+  };
+  
+
+  // Function to extract the file name from the key
+  const extractFileName = (key) => {
+    const lastIndex = key.lastIndexOf("/");
+    return key.substring(lastIndex + 1);
   };
 
-  const handleRemoveFile = (fileId) => {
-    // Implement the logic to remove the file based on fileId
-    console.log("File removed successfully:", fileId);
+  const fetchFiles = async () => {
+    try {
+      // Use Storage.list to get a list of objects in the project folder
+      const response = await Storage.list(`projects/${realProjectId}/`);
+
+      // Check if response.results is an array
+      if (Array.isArray(response.results)) {
+        // Use Promise.all to fetch additional details for each file
+        const filesInFolder = await Promise.all(
+          response.results.map(async (file) => {
+            const details = await Storage.get(file.key);
+
+            return {
+              key: file.key,
+              size: details.size, // Add size field
+              lastModified: details.lastModified, // Add lastModified field
+              ...details,
+            };
+          })
+        );
+
+        console.log("Files in folder:", filesInFolder); // Log the result
+
+        // Update the state with the fetched files
+        setFiles(filesInFolder);
+      } else {
+        console.error("Error fetching files: Invalid response format");
+      }
+    } catch (error) {
+      console.error("Error fetching files:", error);
+    }
   };
+
+  useEffect(() => {
+    fetchFiles(); // Fetch files when the component mounts
+  }, []); // Empty dependency array to ensure it only runs once
+
+  const handleDeleteIconClick = async () => {
+    try {
+      console.log("Selected files to delete:", selectedFiles);
+
+      // Assuming you have a function to remove files
+      // handleRemoveFile function needs to be implemented
+      await Promise.all(
+        selectedFiles.map(async (fileKey) => {
+          await Storage.remove(fileKey); // Remove the file from S3
+          handleRemoveFile(fileKey);
+        })
+      );
+
+      // Clear selected files after deletion
+      setSelectedFiles([]);
+
+      // Fetch files after deletion
+      await fetchFiles();
+    } catch (error) {
+      console.error("Error deleting files:", error);
+    }
+  };
+
+  const handleDownloadIconClick = async () => {
+    try {
+      console.log("Selected files to download:", selectedFiles);
+
+      // Assuming you have a function to download files
+      // handleDownloadFile function needs to be implemented
+      const downloadPromises = selectedFiles.map(async (fileKey) => {
+        try {
+          const fileName = extractFileName(fileKey);
+          const downloadUrl = await Storage.get(
+            `projects/${realProjectId}/${fileName}`
+          );
+          return downloadUrl;
+        } catch (storageError) {
+          console.error(
+            "Error getting download URL from Storage:",
+            storageError
+          );
+          return null;
+        }
+      });
+
+      const downloadUrls = await Promise.all(downloadPromises);
+
+      console.log("Download URLs:", downloadUrls);
+
+      // Automatically initiate download for each file
+      for (const downloadUrl of downloadUrls) {
+        if (downloadUrl) {
+          window.open(downloadUrl, "_blank");
+        }
+      }
+    } catch (error) {
+      console.error("Error downloading files:", error);
+    }
+  };
+
+  const handleRemoveFile = (fileKey) => {
+    // Implement the logic to remove the file based on fileKey
+    console.log("File removed successfully:", fileKey);
+  };
+
+  // const handleDownloadFile = (downloadUrl) => {
+  //   // Implement the logic to handle file download
+  //   console.log("Downloading file from:", downloadUrl);
+  //   // You can use window.open(downloadUrl) or other methods to initiate the download
+  // };
 
   const isSmallScreen = useMediaQuery(theme.breakpoints.down("sm"));
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="xl" sx={{ background: "#090810", marginTop: "0rem", paddingTop: "2rem" }}>
+      <Container
+        maxWidth="xl"
+        sx={{ background: "#090810", marginTop: "0rem", paddingTop: "2rem" }}
+      >
         <Container maxWidth="md">
           <Box
             style={{
@@ -93,12 +240,25 @@ function Files({ projectId, realProjectId }) {
               style={{ marginLeft: "auto" }}
             >
               Choose File
-              <input type="file" onChange={(event) => handleFileUpload(event, realProjectId)} style={{ display: "none" }} />
+              <input
+                type="file"
+                onChange={async (event) => {
+                  await handleFileUpload(event, realProjectId);
+                  await fetchFiles(); // Fetch files after upload
+                }}
+                style={{ display: "none" }}
+              />
             </Button>
           </Box>
 
           {/* Download and Delete Icons */}
-          <div style={{ display: "flex", alignItems: "center", marginBottom: "1rem" }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              marginBottom: "1rem",
+            }}
+          >
             {isSmallScreen ? (
               <>
                 <IconButton
@@ -109,7 +269,9 @@ function Files({ projectId, realProjectId }) {
                 </IconButton>
                 <IconButton
                   style={{ color: "#FFFFFF", cursor: "pointer" }}
-                  onClick={handleDeleteIconClick}
+                  onChange={async (event) => {
+                    await handleDeleteIconClick(event);
+                  }}
                 >
                   <DeleteIcon />
                 </IconButton>
@@ -117,7 +279,11 @@ function Files({ projectId, realProjectId }) {
             ) : (
               <>
                 <IconButton
-                  style={{ color: "#FFFFFF", cursor: "pointer", marginRight: "8px" }}
+                  style={{
+                    color: "#FFFFFF",
+                    cursor: "pointer",
+                    marginRight: "8px",
+                  }}
                   onClick={handleDeleteIconClick}
                 >
                   <DeleteIcon />
@@ -143,32 +309,37 @@ function Files({ projectId, realProjectId }) {
           >
             {isSmallScreen && (
               <Checkbox
-              color="default"
-              style={{ marginRight: "8px" }}
-              checked={selectedFiles.length === files.length}
-              onChange={() =>
-                setSelectedFiles(
-                  selectedFiles.length === files.length
-                    ? []
-                    : files.map((file) => file.id)
-                )
-              }
+                color="default"
+                style={{ marginRight: "8px" }}
+                checked={selectedFiles.length === files.length}
+                onChange={() =>
+                  setSelectedFiles(
+                    selectedFiles.length === files.length
+                      ? []
+                      : files.map((file) => ({ ...file, id: file.key }))
+                  )
+                }
               />
             )}
             <ListItemText primary="Name" sx={{ color: "#FFFFFF", flex: 1 }} />
             {!isSmallScreen && (
               <>
-                <ListItemText primary="Duration" sx={{ color: "#FFFFFF", flex: 1 }} />
-                <ListItemText primary="Size" sx={{ color: "#FFFFFF", flex: 1 }} />
-                <ListItemText primary="Upload" sx={{ color: "#FFFFFF", flex: 1 }} />
+                {/* <ListItemText
+                  primary="Size"
+                  sx={{ color: "#FFFFFF", flex: 1 }}
+                />
+                <ListItemText
+                  primary="Last Modified"
+                  sx={{ color: "#FFFFFF", flex: 1 }}
+                /> */}
               </>
             )}
           </ListItem>
           <Divider sx={{ backgroundColor: "#FFFFFF" }} />
 
           {/* File List */}
-          {files.map((file) => (
-            <React.Fragment key={file.id}>
+          {files.map((file, index) => (
+            <React.Fragment key={index}>
               <ListItem
                 sx={{
                   background: "#090810",
@@ -178,20 +349,38 @@ function Files({ projectId, realProjectId }) {
                 }}
               >
                 <Checkbox
-                    color="default"
-                    style={{ marginRight: "8px" }}
-                    checked={selectedFiles.includes(file.id)}
-                    onChange={() => handleCheckboxChange(file.id)}
-                  />
-                <ListItemText primary={file.name} sx={{ color: "#FFFFFF", flex: 1 }} />
+                  color="default"
+                  style={{ marginRight: "8px" }}
+                  checked={selectedFiles.includes(file.key)}
+                  onChange={() => handleCheckboxChange(file.key)}
+                />
+                <ListItemText
+                  primary={extractFileName(file.key)}
+                  sx={{ color: "#FFFFFF", flex: 1 }}
+                />
+                 {isAudioFile(file.key) && (
+                <IconButton
+                  style={{ color: "#FFFFFF", cursor: "pointer" }}
+                  onClick={() => handlePlayPause(file.key)}
+                >
+                  {playingFile === file.key ? <PauseIcon /> : <PlayArrowIcon />}
+                </IconButton>
+              )}
                 {!isSmallScreen && (
                   <>
-                    <ListItemText primary={file.duration} sx={{ color: "#FFFFFF", flex: 1 }} />
-                    <ListItemText primary={file.size} sx={{ color: "#FFFFFF", flex: 1 }} />
-                    {/* Placeholder for Upload button */}
-                    <ListItemText primary="Upload Button" sx={{ color: "#FFFFFF", flex: 1 }} />
+                    <ListItemText
+                      primary={file.lastModified}
+                      sx={{ color: "#FFFFFF", flex: 1 }}
+                    />
+                    <ListItemText
+                      primary={file.size}
+                      sx={{ color: "#FFFFFF", flex: 1 }}
+                    />
                   </>
                 )}
+                {/* Add console.log statements here to check values */}
+                {console.log("File size:", file.size)}
+                {console.log("File lastModified:", file.lastModified)}
               </ListItem>
               <Divider sx={{ backgroundColor: "#FFFFFF" }} />
             </React.Fragment>
